329a9cb4854238c02c1ac8e399e708ce0ffc97a3
Akhil Talashi <talashi.a@northeastern.edu>
2024-11-15 01:59:05 -0500

    changes for deployment

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./routes/index.js";
+import * as dotenv from "dotenv";  // Import dotenv
+
+dotenv.config();  // Load environment variables
 
 const app = express();
 app.use(cors());
@@ -13,11 +16,17 @@ app.listen(8000, () => {
   console.log("Server started on port 8000!");
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/leetifydb");
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
-const db = mongoose.connection;
+// mongoose.connect("mongodb://127.0.0.1:27017/leetifydb");
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => console.log("MongoDB connected successfully!"));
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => console.log("MongoDB connected successfully!"));
 
 export default app;
